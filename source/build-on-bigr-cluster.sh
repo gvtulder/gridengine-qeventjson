25f799c9538bf94bf92f5cd2fb6c49a2557d0ff2
Gijs van Tulder <gvtulder@gmail.com>
2015-05-29 17:53:30 +0200

    Apparently export -x doesn't work.

--- source/build-on-bigr-cluster.sh
+++ source/build-on-bigr-cluster.sh
@@ -11,9 +11,9 @@ rpm2cpio pam-devel-1.1.1-20.el6.x86_64.rpm | cpio -ivdm
 cd ../
 
 extra_lib_dir=$(pwd)/extra-lib
-export -x LIBRARY_PATH="$LIBRARY_PATH:${extra_lib_dir}/usr/lib64/"
-export -x LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${extra_lib_dir}/usr/lib64/"
-export -x C_INCLUDE_PATH="$C_INCLUDE_PATH:${extra_lib_dir}/usr/include/"
+export LIBRARY_PATH="$LIBRARY_PATH:${extra_lib_dir}/usr/lib64/"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${extra_lib_dir}/usr/lib64/"
+export C_INCLUDE_PATH="$C_INCLUDE_PATH:${extra_lib_dir}/usr/include/"
 
 ./aimk -only-depend -no-java -no-jni -no-secure -no-dump
 scripts/zerodepend
