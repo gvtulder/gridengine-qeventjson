125c160036b327fe2e334b46779b17dd6d12fafb
Gijs van Tulder <gvtulder@gmail.com>
2015-05-29 17:41:43 +0200

    Add build script for BIGR cluster.

---
+++ source/build-on-bigr-cluster.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# build on the BIGR cluster
+
+# pam and pam-devel are not currently installed on the cluster
+mkdir extra-lib/
+cd extra-lib/
+wget http://mirror.nucleus.be/scientific/6x/x86_64/os/Packages/pam-1.1.1-20.el6.x86_64.rpm
+wget http://mirror.nucleus.be/scientific/6x/x86_64/os/Packages/pam-devel-1.1.1-20.el6.x86_64.rpm
+rpm2cpio pam-1.1.1-20.el6.x86_64.rpm | cpio -ivdm
+rpm2cpio pam-devel-1.1.1-20.el6.x86_64.rpm | cpio -ivdm
+cd ../
+
+extra_lib_dir=$(pwd)/extra-lib
+export -x LIBRARY_PATH="$LIBRARY_PATH:${extra_lib_dir}/usr/lib64/"
+export -x LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${extra_lib_dir}/usr/lib64/"
+export -x C_INCLUDE_PATH="$C_INCLUDE_PATH:${extra_lib_dir}/usr/include/"
+
+./aimk -only-depend -no-java -no-jni -no-secure -no-dump
+scripts/zerodepend
+./aimk -no-java -no-jni -no-secure -only-core -no-dump depend
+./aimk -no-java -no-jni -no-secure -only-core -no-dump
+
